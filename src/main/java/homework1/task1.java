562454a00ca04f74f3fca39c899b17946e09bfc9
Artem Oleynikov <allacrit@yandex.ru>
2023-04-30 16:36:58 +0300

    End

--- src/main/java/homework1/task1.java
+++ src/main/java/homework1/task1.java
@@ -1,4 +1,27 @@
+//Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
 package homework1;
 
+import java.util.Scanner;
+
 public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        int i = scanner.nextInt();
+        scanner.close();
+        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        System.out.printf("Факториал числа: %d\n", getFactorial(i));
+    }
+
+    public static int giveMeNumber(int a) {
+        return (a * (a + 1)) / 2;
+    }
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
 }
--- src/main/java/homework1/task3.java
+++ src/main/java/homework1/task3.java
@@ -1,4 +1,59 @@
+//Реализовать простой калькулятор
+
 package homework1;
 
+import java.util.Scanner;
+
 public class task3 {
+    private static String operations;
+    private static int result;
+    private static int numberOne;
+    private static int numberTwo;
+
+    public static void main(String[] args) {
+        System.out.print("Введите первое число: ");
+        numberOne = scanNumber();
+        System.out.print("Введите второе число: ");
+        numberTwo = scanNumber();
+        System.out.println("Введите оператор: -, +, *, /");
+        operations = scanOperations();
+        result = getResult();
+        System.out.printf("%d %s %d = %d", numberOne, operations, numberTwo, result);
+    }
+
+    static int scanNumber() {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        return number;
+    }
+
+    static String scanOperations() {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        return str;
+    }
+
+    static int getResult() {
+        switch (operations) {
+            case "+":
+                result = numberOne + numberTwo;
+                break;
+            case "-":
+                result = numberOne - numberTwo;
+                break;
+            case "*":
+                result = numberOne * numberTwo;
+                break;
+            case "/":
+                result = numberOne / numberTwo;
+                break;
+            default:
+                System.out.println("!!!НЕ ПРАВИЛЬНЫЙ ОПЕРАТОР!!!");
+                System.out.println("Введите оператор: -, +, *, /");
+                operations = scanOperations();
+                getResult();
+                break;
+        }
+        return result;
+    }
 }
